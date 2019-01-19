b3648a437ec24c2422b7a5c45e8f3828e12c6369
Ismail H Rana <mranakuet@gmail.com>
2019-01-19 21:44:11 +0600

    Brand Updated

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,50 +1,12 @@
-// Sticky Header
-$(window).scroll(function() {
-
-    if ($(window).scrollTop() > 60) {
-        $('.main_h').addClass('sticky');
-    } else {
-        $('.main_h').removeClass('sticky');
-    }
-});
-
-// Mobile Navigation
-$('.mobile-toggle').click(function() {
-    if ($('.main_h').hasClass('open-nav')) {
-        $('.main_h').removeClass('open-nav');
-    } else {
-        $('.main_h').addClass('open-nav');
-        $('.main_h').addClass('sticky');
-    }
-});
-
-$('.main_h li a').click(function() {
-    if ($('.main_h').hasClass('open-nav')) {
-        $('.navigation').removeClass('open-nav');
-        $('.main_h').removeClass('open-nav');
-    }
-});
-
-// navigation scroll lijepo radi materem
-$('nav a').click(function(event) {
-    var id = $(this).attr("href");
-    var offset = 70;
-    var target = $(id).offset().top - offset;
-    $('html, body').animate({
-        scrollTop: target
-    }, 500);
-    event.preventDefault();
-});
-
 // AOS active
 AOS.init({
     disable: window.innerWidth < 1024,
     duration: 600,
-    easing: 'ease-out',
-    once: true
+    easing: 'ease-out'
+    // once: true
 });
 
-// sm
+// sm Animation
 function makeTween(selector) {
     var $q = $(selector);
     var timeline = new TimelineMax();
